076159a26c07aade9480736ca3f1594c38893f93
Kris <neugodova_kr@mail.ru>
2023-01-17 20:59:07 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+class TestData:
+
+
+    base_url = 'https://b2c.passport.rt.ru'
+
+    valid_email = 'neugodova4666@mail.ru'
+    invalid_email = 'neugodova4666@mail.ru@'
+    password1 = 'AAAAAAa1'
+    password2 = 'AAAAAAa2'
+    password3 = 'AAAAAAa3'
+    short_password = 'A1'
+    lowercase_password = 'aaaaa2'
+    cyrillic_password = 'Aаббввгг2'
+    empty_password = ''
+    first_name_positive1 = 'Tест'
+    first_name_positive2 = '--'
+    last_name_positive1 = 'Tест'
+    last_name_positive2 = 'Ростелеком'
+    valid_phone = '+79779431730'
+    invalid_phone = '+7977a431730'
